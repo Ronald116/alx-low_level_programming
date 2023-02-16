75a16d0c9333df8869f84d66971df1f93ddebd31
Ronald116 <ronaldbotchway@gmail.com>
2023-02-16 10:42:12 -0800

    4-puts.c

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Using the puts function to print a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
