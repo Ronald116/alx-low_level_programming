baed38f8096de14c90ad25ce625f7ad044db193a
Ronald116 <ronaldbotchway@gmail.com>
2023-02-18 07:06:00 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
