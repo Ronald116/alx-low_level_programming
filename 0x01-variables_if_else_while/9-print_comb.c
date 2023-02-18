28b1e4f5f63420793df43c3ebd145d53dc52378a
Ronald116 <ronaldbotchway@gmail.com>
2023-02-18 09:30:24 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -  combination of single digits numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = '0';
+	
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+		
+	
