31a95327c0213b9e21c1ab45ed28ba37423796c5
Ronald116 <ronaldbotchway@gmail.com>
2023-02-18 08:41:22 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
