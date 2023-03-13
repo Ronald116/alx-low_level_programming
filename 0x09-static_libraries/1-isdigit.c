81f597898fa6342626cac853c13ea2c7ba64c49c
Ronald116 <ronaldbotchway@gmail.com>
2023-03-13 06:49:41 -0700

    updated

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit -  checks for a dogit
+ * @c: int input
+ * Return: Always 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy -  aftn that copies a memory area
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: Always 0
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
