9968eafa1f754190a439828d849396dd9dcdce5c
Ronald116 <ronaldbotchway@gmail.com>
2023-05-18 08:04:01 -0700

    TASK 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_dlistint - a ftn that prints all elements
+ * @h: head input
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int num_nodes = 0;
+
+
+	if (h == NULL)
+	{
+		return (h);
+	}
+
+	else
+	{
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			num_nodes++;
+		}
+	}
+
+	return (num_nodes);
+}
