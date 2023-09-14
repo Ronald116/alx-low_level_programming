b31df7b1053121c6c4b62f7f91349272526cb480
Ronald116 <ronaldbotchway@gmail.com>
2023-09-14 09:34:06 -0700

    betty checks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 
 /**
- * linear_searh - a function that searches for a value using linear
- * search algorithm
+ * linear_searh - searches for a value using linear search algorithm
  * @array: pointer to the first element
  * @size: number of elements in the array
  * @value: value to be searched
@@ -13,23 +12,23 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-        size_t i;
+	size_t i;
 
-        if (array == NULL)
-        {
-                return (-1);
-        }
+	if (array == NULL)
+	{
+		return (-1);
+	}
 
-        for (i = 0; i < size; i++)
-        {
-                printf("Value checked array[%li] = [%i]\n", i, array[i]);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
 
-                if (array[i] == value)
-                {
-                        return (i);
-                }
-        }
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
 
-        return (-1);
+	return (-1);
 }
 
